2ec8574d08ac2a4525ecfe36b2deb4a841741729
VovaRomaniuk <romaniukvova@gmail.com>
2017-04-04 18:37:31 +0300

    Android Message Handler added (RevSDKMessageHandler proj in RevApm)

--- RevAPMiOSSDK/RevApm/Facade.cs
+++ RevAPMiOSSDK/RevApm/Facade.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using System.Net;
 using System.Net.Http;
 using System.IO;
+using System.Threading;
 
 namespace RevApm
 {
@@ -51,6 +52,12 @@ namespace RevApm
     {
         const string wrongVersion = "You're referencing the Portable version in your App - you need to reference the platform (iOS/Android) version";
 
+        public ProgressStreamContent(Stream stream, CancellationToken token)
+           : this(new ProgressStream(stream, token))
+        {
+        }
+
+
         ProgressStreamContent(Stream stream) : base(stream)
         {
             throw new Exception(wrongVersion);
@@ -84,4 +91,109 @@ namespace RevApm
             get { throw new Exception(wrongVersion); }
         }
     }
+
+    class ProgressStream : Stream
+    {
+        CancellationToken token;
+
+        public ProgressStream(Stream stream, CancellationToken token)
+        {
+            ParentStream = stream;
+            this.token = token;
+
+            ReadCallback = delegate { };
+            WriteCallback = delegate { };
+        }
+
+        public Action<long> ReadCallback { get; set; }
+
+        public Action<long> WriteCallback { get; set; }
+
+        public Stream ParentStream { get; private set; }
+
+        public override bool CanRead { get { return ParentStream.CanRead; } }
+
+        public override bool CanSeek { get { return ParentStream.CanSeek; } }
+
+        public override bool CanWrite { get { return ParentStream.CanWrite; } }
+
+        public override bool CanTimeout { get { return ParentStream.CanTimeout; } }
+
+        public override long Length { get { return ParentStream.Length; } }
+
+        public override void Flush()
+        {
+            ParentStream.Flush();
+        }
+
+        public override Task FlushAsync(CancellationToken cancellationToken)
+        {
+            return ParentStream.FlushAsync(cancellationToken);
+        }
+
+        public override long Position
+        {
+            get { return ParentStream.Position; }
+            set { ParentStream.Position = value; }
+        }
+
+        public override int Read(byte[] buffer, int offset, int count)
+        {
+            token.ThrowIfCancellationRequested();
+
+            var readCount = ParentStream.Read(buffer, offset, count);
+            ReadCallback(readCount);
+            return readCount;
+        }
+
+        public override long Seek(long offset, SeekOrigin origin)
+        {
+            token.ThrowIfCancellationRequested();
+            return ParentStream.Seek(offset, origin);
+        }
+
+        public override void SetLength(long value)
+        {
+            token.ThrowIfCancellationRequested();
+            ParentStream.SetLength(value);
+        }
+
+        public override void Write(byte[] buffer, int offset, int count)
+        {
+            token.ThrowIfCancellationRequested();
+            ParentStream.Write(buffer, offset, count);
+            WriteCallback(count);
+        }
+
+        public override async Task<int> ReadAsync(byte[] buffer, int offset, int count, CancellationToken cancellationToken)
+        {
+            token.ThrowIfCancellationRequested();
+            var linked = CancellationTokenSource.CreateLinkedTokenSource(token, cancellationToken);
+
+            var readCount = await ParentStream.ReadAsync(buffer, offset, count, linked.Token);
+
+            ReadCallback(readCount);
+            return readCount;
+        }
+
+        public override Task WriteAsync(byte[] buffer, int offset, int count, CancellationToken cancellationToken)
+        {
+            token.ThrowIfCancellationRequested();
+
+            var linked = CancellationTokenSource.CreateLinkedTokenSource(token, cancellationToken);
+            var task = ParentStream.WriteAsync(buffer, offset, count, linked.Token);
+
+            WriteCallback(count);
+            return task;
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                ParentStream.Dispose();
+            }
+        }
+    }
 }
+
