1a94536ff7997e69050bcd9248eb46a290b3685b
devlanfear <devlanfear@gmail.com>
2017-04-28 13:39:42 +0300

    Test RSS app

---
+++ TestRSSReader/TestRSSReader/TestRSSReader.Android/MainApplication.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Nuubit.SDK;
+
+namespace TestRSSReader.Droid
+{
+    [Application(Debuggable = false)]
+    class MainApplication : Com.Nuubit.Sdk.NuubitApplication
+    {
+        public MainApplication(IntPtr handle, JniHandleOwnership transer)
+            : base(handle, transer)
+        {
+          
+        }
+    }
+}
---
+++ TestRSSReader/TestRSSReader/TestRSSReader/MainPage.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+using Xamarin.Forms;
+
+namespace TestRSSReader
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+
+            var data = await new HttpClient(App.Handler).GetStringAsync("http://rssfeeds.usatoday.com/usatoday-NewsTopStories");
+
+            XDocument document = XDocument.Parse(data);
+
+            var feeds = from feed in document.Descendants("item")
+                select new Item
+                {
+                    Title = feed.Element("title").Value,
+                    Link = feed.Element("link").Value,
+                    Description = feed.Element("description").Value
+                };
+
+            lst.ItemsSource = feeds;
+        }
+
+        private void Lst_OnItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            var item = e.Item as Item;
+
+            Navigation.PushModalAsync(new WebPage(item));
+        }
+    }
+
+    public class Item
+    {
+        public string Title { get; set; }
+
+        public string Link { get; set; }
+
+        public string Description { get; set; }
+    }
+}
