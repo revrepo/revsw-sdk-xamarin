479a781d6707ee7ff7ad0b6d7a19ad6d76e913e0
Mykhail Galushko <devlanfear@gmail.com>
2017-05-01 21:36:32 +0300

    Android binding

--- RacerApp.XamarinForms/RacerMobileApp/Droid/MainApplication.cs
+++ RacerApp.XamarinForms/RacerMobileApp/Droid/MainApplication.cs
@@ -16,71 +16,82 @@ using Java.Interop;
 
 namespace RacerMobileAppNew.Droid
 {
- //   [Application(Debuggable = false)]
-	//class MainApplication : Com.Nuubit.Sdk.NuubitApplication
- //   {
- //       public MainApplication(IntPtr handle, JniHandleOwnership transer)
- //          : base(handle, transer)
- //       {
- //       }
- //   }
-[Register("android/support/multidex/MultiDexApplication", DoNotGenerateAcw = true)]
-public class MultiDexApplication : Com.Nuubit.Sdk.NuubitApplication
-{
-	internal static readonly JniPeerMembers _members =
-		new XAPeerMembers("android/support/multidex/MultiDexApplication", typeof(MultiDexApplication));
+    [Application(Debuggable = false)]
+	class MainApplication : Com.Nuubit.Sdk.NuubitApplication
+    {
+        public MainApplication(IntPtr handle, JniHandleOwnership transer)
+           : base(handle, transer)
+        {
+        }
 
-	internal static IntPtr java_class_handle;
-
-	private static IntPtr id_ctor;
-
-	[Register(".ctor", "()V", "", DoNotGenerateAcw = true)]
-	public MultiDexApplication()
-		: base(IntPtr.Zero, JniHandleOwnership.DoNotTransfer)
-	{
-		if (Handle != IntPtr.Zero)
-			return;
-
-		try
+		public override void OnCreate()
 		{
-			if (GetType() != typeof(MultiDexApplication))
-			{
-				SetHandle(
-					JNIEnv.StartCreateInstance(GetType(), "()V"),
-					JniHandleOwnership.TransferLocalRef);
-				JNIEnv.FinishCreateInstance(Handle, "()V");
-				return;
-			}
-
-			if (id_ctor == IntPtr.Zero)
-				id_ctor = JNIEnv.GetMethodID(class_ref, "&lt;init&gt;", "()V");
-			SetHandle(
-				JNIEnv.StartCreateInstance(class_ref, id_ctor),
-				JniHandleOwnership.TransferLocalRef);
-			JNIEnv.FinishCreateInstance(Handle, class_ref, id_ctor);
+			base.OnCreate();
 		}
-		finally
-		{
-		}
-	}
+ 
+    }
+//[Register("android/support/multidex/MultiDexApplication", DoNotGenerateAcw = true)]
+//public class MultiDexApplication : Com.Nuubit.Sdk.NuubitApplication
+//{
+//	internal static readonly JniPeerMembers _members =
+//		new XAPeerMembers("android/support/multidex/MultiDexApplication", typeof(MultiDexApplication));
 
-	protected MultiDexApplication(IntPtr javaReference, JniHandleOwnership transfer)
-		: base(javaReference, transfer)
-	{
-	}
+//	internal static IntPtr java_class_handle;
 
-	internal static IntPtr class_ref
-	{
-		get { return JNIEnv.FindClass("android/support/multidex/MultiDexApplication", ref java_class_handle); }
-	}
+//	private static IntPtr id_ctor;
 
-	protected override IntPtr ThresholdClass
-	{
-		get { return class_ref; }
-	}
+//		public override void OnCreate()
+//		{
+//			base.OnCreate();
+//		}
 
-	protected override Type ThresholdType
-	{
-		get { return typeof(MultiDexApplication); }
-	} }
+//	[Register(".ctor", "()V", "", DoNotGenerateAcw = true)]
+//	public MultiDexApplication()
+//		: base(IntPtr.Zero, JniHandleOwnership.DoNotTransfer)
+//	{
+//		if (Handle != IntPtr.Zero)
+//			return;
+
+//		try
+//		{
+//			if (GetType() != typeof(MultiDexApplication))
+//			{
+//				SetHandle(
+//					JNIEnv.StartCreateInstance(GetType(), "()V"),
+//					JniHandleOwnership.TransferLocalRef);
+//				JNIEnv.FinishCreateInstance(Handle, "()V");
+//				return;
+//			}
+
+//			if (id_ctor == IntPtr.Zero)
+//				id_ctor = JNIEnv.GetMethodID(class_ref, "&lt;init&gt;", "()V");
+//			SetHandle(
+//				JNIEnv.StartCreateInstance(class_ref, id_ctor),
+//				JniHandleOwnership.TransferLocalRef);
+//			JNIEnv.FinishCreateInstance(Handle, class_ref, id_ctor);
+//		}
+//		finally
+//		{
+//		}
+//	}
+
+//	protected MultiDexApplication(IntPtr javaReference, JniHandleOwnership transfer)
+//		: base(javaReference, transfer)
+//	{
+//	}
+
+//	internal static IntPtr class_ref
+//	{
+//		get { return JNIEnv.FindClass("android/support/multidex/MultiDexApplication", ref java_class_handle); }
+//	}
+
+//	protected override IntPtr ThresholdClass
+//	{
+//		get { return class_ref; }
+//	}
+
+//	protected override Type ThresholdType
+//	{
+//		get { return typeof(MultiDexApplication); }
+//	} }
 }
