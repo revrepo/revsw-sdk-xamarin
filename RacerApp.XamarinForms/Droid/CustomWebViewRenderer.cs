9281d68ff70ed5f824f19b90b07e9f160ac8ca00
Mykhail Galushko <devlanfear@gmail.com>
2017-05-02 10:08:25 +0300

    Refactor

---
+++ RacerApp.XamarinForms/Droid/CustomWebViewRenderer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using RacerMobileApp;
+using RacerMobileAppUpd.Droid;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+
+
+[assembly: ExportRenderer(typeof(CustomWebView), typeof(CustomWebViewRenderer))]
+namespace  RacerMobileAppUpd.Droid
+{
+    public class CustomWebViewRenderer : WebViewRenderer
+    {
+        protected override async void OnElementChanged(ElementChangedEventArgs<Xamarin.Forms.WebView> e)
+        {
+            base.OnElementChanged(e);
+
+            var webView = Control;
+            var chromeClient = Com.Nuubit.Sdk.NuubitSDK.CreateWebChromeClient();
+            webView.SetWebChromeClient(chromeClient);
+            webView.SetWebViewClient(Com.Nuubit.Sdk.NuubitSDK.CreateWebViewClient(Context, webView, Com.Nuubit.Sdk.NuubitSDK.OkHttpCreate(Com.Nuubit.Sdk.NuubitConstants.DefaultTimeoutSec, false, false)));
+
+        }
+
+
+    }
+}
---
+++ RacerApp.XamarinForms/Droid/MainApplication.cs
@@ -0,0 +1,97 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Nuubit.SDK;
+using RacerMobileApp.Services;
+using Android.Runtime;
+using Java.Interop;
+
+namespace RacerMobileAppUpd.Droid
+{
+    [Application(Debuggable = false)]
+	class MainApplication : Com.Nuubit.Sdk.NuubitApplication
+    {
+        public MainApplication(IntPtr handle, JniHandleOwnership transer)
+           : base(handle, transer)
+        {
+        }
+
+		public override void OnCreate()
+		{
+			base.OnCreate();
+		}
+ 
+    }
+//[Register("android/support/multidex/MultiDexApplication", DoNotGenerateAcw = true)]
+//public class MultiDexApplication : Com.Nuubit.Sdk.NuubitApplication
+//{
+//	internal static readonly JniPeerMembers _members =
+//		new XAPeerMembers("android/support/multidex/MultiDexApplication", typeof(MultiDexApplication));
+
+//	internal static IntPtr java_class_handle;
+
+//	private static IntPtr id_ctor;
+
+//		public override void OnCreate()
+//		{
+//			base.OnCreate();
+//		}
+
+//	[Register(".ctor", "()V", "", DoNotGenerateAcw = true)]
+//	public MultiDexApplication()
+//		: base(IntPtr.Zero, JniHandleOwnership.DoNotTransfer)
+//	{
+//		if (Handle != IntPtr.Zero)
+//			return;
+
+//		try
+//		{
+//			if (GetType() != typeof(MultiDexApplication))
+//			{
+//				SetHandle(
+//					JNIEnv.StartCreateInstance(GetType(), "()V"),
+//					JniHandleOwnership.TransferLocalRef);
+//				JNIEnv.FinishCreateInstance(Handle, "()V");
+//				return;
+//			}
+
+//			if (id_ctor == IntPtr.Zero)
+//				id_ctor = JNIEnv.GetMethodID(class_ref, "&lt;init&gt;", "()V");
+//			SetHandle(
+//				JNIEnv.StartCreateInstance(class_ref, id_ctor),
+//				JniHandleOwnership.TransferLocalRef);
+//			JNIEnv.FinishCreateInstance(Handle, class_ref, id_ctor);
+//		}
+//		finally
+//		{
+//		}
+//	}
+
+//	protected MultiDexApplication(IntPtr javaReference, JniHandleOwnership transfer)
+//		: base(javaReference, transfer)
+//	{
+//	}
+
+//	internal static IntPtr class_ref
+//	{
+//		get { return JNIEnv.FindClass("android/support/multidex/MultiDexApplication", ref java_class_handle); }
+//	}
+
+//	protected override IntPtr ThresholdClass
+//	{
+//		get { return class_ref; }
+//	}
+
+//	protected override Type ThresholdType
+//	{
+//		get { return typeof(MultiDexApplication); }
+//	} }
+}
